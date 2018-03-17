68cdb983f9a00db374d285a543da9a46444dfce9
Stefan Majewsky <majewsky@gmx.net>
2018-03-17 22:17:36 +0100

    minimal example for tokio-uds
    This would usually have no place in here as a separate commit, but
    considering the amount of fighting it took me to get to this point, this
    needs to be conserved.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+/*******************************************************************************
+*
+* Copyright 2018 Stefan Majewsky <majewsky@gmx.net>
+*
+* This program is free software: you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the Free Software
+* Foundation, either version 3 of the License, or (at your option) any later
+* version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+* A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*******************************************************************************/
+
+extern crate tokio;
+extern crate tokio_core;
+extern crate tokio_uds;
+
+use tokio::prelude::*;
+use tokio_core::reactor::Core;
+use tokio_uds::UnixListener;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        println!("FATAL: {}", err);
+    }
+}
+
+struct CleanupSocketOnExit<'a>(&'a std::path::Path);
+
+impl<'a> Drop for CleanupSocketOnExit<'a> {
+    fn drop(&mut self) {
+        std::fs::remove_file(self.0).expect("Socket cleanup failed");
+    }
+}
+
+fn run() -> std::io::Result<()> {
+    let mut core = Core::new().expect("Core::new failed");
+    let handle = core.handle();
+
+    //TODO: the Drop handler does not run when SIGINT/SIGTERM terminates the program
+    let socket_path = CleanupSocketOnExit(std::path::Path::new("./vt6term"));
+    let listener = UnixListener::bind(socket_path.0, &handle)?;
+
+    let server = listener.incoming().for_each(|(stream, _addr)| {
+        let (reader, writer) = stream.split();
+        tokio::spawn(
+            tokio::io::copy(reader, writer)
+                .map(|_| ())
+                .map_err(|err| { println!("ERROR copy: {}", err); () })
+        );
+        Ok(())
+    }).map_err(|err| {
+        println!("ERROR listener.incoming.for_each: {}", err);
+    });
+
+    core.run(server).expect("Event loop failed");
+    Ok(())
 }
