ec914dd54a0cf9c06a6fdc904a4b1bc2bcf1aa2f
Stefan Majewsky <majewsky@gmx.net>
2018-07-10 19:59:14 +0200

    launch a shell

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ extern crate gtk;
 extern crate libc;
 #[macro_use]
 extern crate log;
+extern crate nix;
 extern crate pango;
 extern crate pangocairo;
 extern crate simple_logger;
@@ -63,7 +64,7 @@ fn main() {
 
 
     let socket_path = std::path::PathBuf::from("./vt6term");
-    let server = match server::Server::new(socket_path, event_rx, model.clone()) {
+    let server = match server::Server::new(socket_path.clone(), event_rx, model.clone()) {
         Ok(s) => s,
         Err(e) => {
             error!("failed to initialize VT6 server socket: {}", e);
@@ -71,8 +72,9 @@ fn main() {
         },
     };
 
-    //TODO: shutdown this thread when the GUI thread is done
-    let join_handle = std::thread::spawn(move || {
+    let mut win = window::Window::new();
+
+    let join_handle1 = std::thread::spawn(move || {
         use futures::Future;
         use tokio::runtime::Runtime;
 
@@ -81,7 +83,56 @@ fn main() {
         rt.shutdown_now().wait().unwrap();
     });
 
-    window::main(&mut event_tx, model);
+    let join_handle2 = std::thread::spawn(move || {
+        let result = spawn_client(socket_path, vec!["/bin/sh".into(), "-i".into()]);
+        use nix::sys::wait::WaitStatus::*;
+        match result {
+            Err(e) => error!("spawn_client failed: {}", e),
+            Ok(Exited(_, code)) => info!("client exited with status {}", code),
+            Ok(Signaled(_, code, _)) => info!("client killed by signal {:?}", code),
+            _ => info!("client watcher returned with status {:?}", result),
+        }
+    });
+
+    win.main(&mut event_tx, model);
     std::mem::drop(event_tx); //signal to server future to shutdown
-    join_handle.join().unwrap();
+    join_handle1.join().unwrap();
+    join_handle2.join().unwrap();
+}
+
+use std::ffi::CString;
+
+fn spawn_client(socket_path: std::path::PathBuf, command_and_args: Vec<String>) -> nix::Result<nix::sys::wait::WaitStatus> {
+    //before forking, make all necessary allocations
+    let env: Vec<CString> = std::env::vars()
+        .filter(|(k,_v)| k != "SHELL" && k != "LINES" && k != "COLUMNS")
+        .map(|(k,v)| CString::new(format!("{}={}", k, v)).unwrap())
+        .collect();
+    let args: Vec<CString> = command_and_args.iter().map(|s| CString::new(s.clone()).unwrap()).collect();
+    let command = CString::new(command_and_args[0].clone()).unwrap();
+
+    //open stdio for the child process
+    use std::os::unix::io::IntoRawFd;
+    use std::os::unix::net::UnixStream;
+    let stream = UnixStream::connect(socket_path).unwrap();
+
+    use nix::unistd::*;
+    match fork()? {
+        ForkResult::Parent { child, .. } => {
+            //wait on child
+            nix::sys::wait::waitpid(child, None)
+        },
+        ForkResult::Child => {
+            let stream_fd = stream.into_raw_fd();
+            dup2(stream_fd, 0).unwrap_or_else(|_| std::process::exit(200));
+            dup2(stream_fd, 1).unwrap_or_else(|_| std::process::exit(201));
+            dup2(stream_fd, 2).unwrap_or_else(|_| std::process::exit(202));
+            if stream_fd > 2 {
+                close(stream_fd).unwrap_or_else(|_| std::process::exit(203));
+            }
+            execve(&command, &args, &env).unwrap_or_else(|_| std::process::exit(204));
+            //FIXME remove the next line when rustc learns to understand that Void can cast into anything
+            Ok(nix::sys::wait::WaitStatus::StillAlive)
+        },
+    }
 }
--- src/window.rs
+++ src/window.rs
@@ -24,7 +24,7 @@ use futures::sync::mpsc;
 use fragile::Sticky;
 use gdk;
 use glib;
-use gtk::{self, DrawingArea, Window, WindowType};
+use gtk::{self, DrawingArea, Window as GtkWindow, WindowType};
 use gtk::prelude::*;
 use libc;
 
@@ -32,79 +32,93 @@ use model;
 use server;
 use view;
 
-///Returns when the GUI thread is done, meaning that all other threads shall be shut down.
-pub fn main(_tx: &mut mpsc::Sender<server::Event>, model: Arc<Mutex<model::Document>>) {
-    gtk::init().unwrap();
+pub struct Window {
+    window: GtkWindow,
+    area: Rc<DrawingArea>,
+}
 
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("6term");
-    let area = Rc::new(DrawingArea::new());
-    window.add(area.as_ref());
-    window.show_all();
+impl Window {
+    pub fn new() -> Window {
+        gtk::init().unwrap();
 
-    //setup SIGUSR1 as a hacky means for the Tokio eventloop to notify this
-    //thread to redraw the GUI (TODO: replace by something less hacky maybe)
-    let area_ref = Sticky::new(area.clone());
-    glib::source::unix_signal_add(libc::SIGUSR1, move || {
-        area_ref.get().queue_draw();
-        glib::Continue(true)
-    });
-
-    window.connect_delete_event(|_,_| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    let view = Rc::new(RefCell::new(
-        view::Document::new(model.clone()),
-    ));
-
-    area.connect_draw(move |widget, cairo_ctx| {
-        view.borrow_mut().render(widget, cairo_ctx);
-        Inhibit(false)
-    });
-
-    area.add_events(gdk::EventMask::KEY_PRESS_MASK.bits() as i32);
-    area.connect_key_press_event(move |widget, event| {
-        let keyval = event.get_keyval();
-        let action = match gdk::keyval_to_unicode(keyval) {
-            //Enter or Return
-            Some('\n') | Some('\r') => model::CursorAction::Insert("\n".into()),
-            //Backspace
-            Some('\u{8}') => model::CursorAction::DeletePreviousChar,
-            //Delete
-            Some('\u{7F}') => model::CursorAction::DeleteNextChar,
-            //printable character
-            Some(ch) if ch as u32 >= 32 => model::CursorAction::Insert(ch.to_string()),
-            //ignore other control characters
-            Some(_) => return Inhibit(false),
-            //other keys
-            None => {
-                use gdk::enums::key;
-                match keyval as key::Key {
-                    key::Left  | key::KP_Left  => model::CursorAction::GotoPreviousChar,
-                    key::Right | key::KP_Right => model::CursorAction::GotoNextChar,
-                    _ => {
-                        info!("unhandled keyval: {}", keyval);
-                        return Inhibit(false);
-                    },
-                }
-            },
+        let w = Window {
+            window: GtkWindow::new(WindowType::Toplevel),
+            area: Rc::new(DrawingArea::new()),
         };
-        let mut document = model.lock().unwrap();
-        if let Some(ref mut p) = document.sections.last_mut() {
-            let need_redraw = p.execute_cursor_action(action);
-            if need_redraw {
-                widget.queue_draw();
+        w.window.set_title("6term");
+        w.window.add(w.area.as_ref());
+        w.window.show_all();
+
+        //setup SIGUSR1 as a hacky means for the Tokio eventloop to notify this
+        //thread to redraw the GUI (TODO: replace by something less hacky maybe)
+        let area_ref = Sticky::new(w.area.clone());
+        glib::source::unix_signal_add(libc::SIGUSR1, move || {
+            area_ref.get().queue_draw();
+            glib::Continue(true)
+        });
+
+        w
+    }
+
+    ///Returns when the GUI thread is done, meaning that all other threads shall be shut down.
+    pub fn main(&mut self, _tx: &mut mpsc::Sender<server::Event>, model: Arc<Mutex<model::Document>>) {
+
+        self.window.connect_delete_event(|_,_| {
+            gtk::main_quit();
+            Inhibit(false)
+        });
+
+        let view = Rc::new(RefCell::new(
+            view::Document::new(model.clone()),
+        ));
+
+        self.area.connect_draw(move |widget, cairo_ctx| {
+            view.borrow_mut().render(widget, cairo_ctx);
+            Inhibit(false)
+        });
+
+        self.area.add_events(gdk::EventMask::KEY_PRESS_MASK.bits() as i32);
+        self.area.connect_key_press_event(move |widget, event| {
+            let keyval = event.get_keyval();
+            let action = match gdk::keyval_to_unicode(keyval) {
+                //Enter or Return
+                Some('\n') | Some('\r') => model::CursorAction::Insert("\n".into()),
+                //Backspace
+                Some('\u{8}') => model::CursorAction::DeletePreviousChar,
+                //Delete
+                Some('\u{7F}') => model::CursorAction::DeleteNextChar,
+                //printable character
+                Some(ch) if ch as u32 >= 32 => model::CursorAction::Insert(ch.to_string()),
+                //ignore other control characters
+                Some(_) => return Inhibit(false),
+                //other keys
+                None => {
+                    use gdk::enums::key;
+                    match keyval as key::Key {
+                        key::Left  | key::KP_Left  => model::CursorAction::GotoPreviousChar,
+                        key::Right | key::KP_Right => model::CursorAction::GotoNextChar,
+                        _ => {
+                            info!("unhandled keyval: {}", keyval);
+                            return Inhibit(false);
+                        },
+                    }
+                },
+            };
+            let mut document = model.lock().unwrap();
+            if let Some(ref mut p) = document.sections.last_mut() {
+                let need_redraw = p.execute_cursor_action(action);
+                if need_redraw {
+                    widget.queue_draw();
+                }
             }
-        }
-        Inhibit(true)
-    });
+            Inhibit(true)
+        });
 
-    area.set_can_focus(true);
-    area.grab_focus();
+        self.area.set_can_focus(true);
+        self.area.grab_focus();
 
-    gtk::main();
+        gtk::main();
+    }
 }
 
 ///Can be called by any thread to trigger a redraw of the GUI.
